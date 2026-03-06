aa8070e0669f8b123c5f7b829d0f04a236064f02
aoshokuhin-cmyk <aoshokuhin@gmail.com>
2026-03-07 01:42:51 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 let inventory = JSON.parse(localStorage.getItem("inventory") || "{}");
 let video;
+let inventoryMode = false;
 
 async function startScan() {
+
   if (!("BarcodeDetector" in window)) {
-    alert("この端末はバーコード読み取り非対応です（Android Chrome推奨）");
+    alert("バーコード非対応");
     return;
   }
 
@@ -23,22 +25,32 @@ async function startScan() {
   await video.play();
 
   const interval = setInterval(async () => {
+
     const codes = await detector.detect(video);
+
     if (codes.length > 0) {
+
       const code = codes[0].rawValue;
+
       stream.getTracks().forEach(t => t.stop());
+
       clearInterval(interval);
+
       video.remove();
+
       addItem(code);
+
     }
+
   }, 500);
+
 }
 
 function addItem(code) {
 
   if (!inventory[code]) {
 
-    let name = prompt("この商品の名前を入力してください");
+    let name = prompt("商品名を入力");
 
     if (!name) name = "商品";
 
@@ -54,27 +66,72 @@ function addItem(code) {
   localStorage.setItem("inventory", JSON.stringify(inventory));
 
   render();
+
 }
 
-function render() {
+function render(list = inventory) {
 
   const log = document.getElementById("log");
 
   log.innerHTML = "<h3>在庫</h3>";
 
+  for (const code in list) {
+
+    const item = list[code];
+
+    log.innerHTML += `
+      <div>
+      ${item.name}
+      <br>
+      ${code}
+      <br>
+      在庫:${item.qty}
+      </div>
+      <hr>
+    `;
+
+  }
+
+}
+
+function searchItem() {
+
+  const word = document.getElementById("search").value.toLowerCase();
+
+  let result = {};
+
   for (const code in inventory) {
 
     const item = inventory[code];
 
-    log.innerHTML += `
-      <div>
-      ${item.name}  
-      (バーコード:${code})  
-      ：${item.qty}
-      </div>
-    `;
+    if (
+      item.name.toLowerCase().includes(word) ||
+      code.includes(word)
+    ) {
+
+      result[code] = item;
+
+    }
 
   }
+
+  render(result);
+
+}
+
+function startInventory() {
+
+  alert("棚卸しモード開始");
+
+  inventoryMode = true;
+
+}
+
+function endInventory() {
+
+  alert("棚卸し終了");
+
+  inventoryMode = false;
 
 }
 
